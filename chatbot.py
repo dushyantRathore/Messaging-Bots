aca5df4642777586b44ecfc5ac894d405ff82885
arjunrajpal <rajpal.arjun@yahoo.in>
2018-01-03 01:37:17 +0530

    updated commands

--- chatbot.py
+++ chatbot.py
@@ -32,8 +32,8 @@ def handle(msg):        # Glance a message
             bot.sendMessage(chat_id, "Welcome to Crypto Bot where you will get all the details about the cryptocurrency market.")
         elif message in greetings:
             bot.sendMessage(chat_id, "Hi")
-        elif "/checkprice" in message:
-            previous_message = "/checkprice"
+        elif "/price" in message:
+            previous_message = "/price"
             # message  = message.split(" ")
             # coin = message[1].upper()
             # price = scraper.get_current_price(coin)
@@ -56,14 +56,14 @@ def handle(msg):        # Glance a message
                 index = str(top_10_exchanges.index(exchange) + 1)
                 response = response + "\n" + index + ". " + exchange
             bot.sendMessage(chat_id, response, parse_mode='Markdown')
-        elif message == "/latestnews":
+        elif message == "/news":
             latest_news = scraper.get_latest_crypto_news()
             bot.sendMessage(chat_id, "*Latest Cryptocurrency news : *", parse_mode="Markdown")
             for news in latest_news:
                 headline = news["headlines"]
                 link = news["link"]
                 bot.sendMessage(chat_id, headline + "\n" + link)
-        elif message == "/mostprofitable":
+        elif message == "/mostprof":
             most_profitable_by_year = {'coin':'', 'diff':-1}
             most_profitable_by_month = {'coin':'', 'diff':-1}
             most_profitable_by_day = {'coin':'', 'diff':-1}
@@ -89,7 +89,7 @@ def handle(msg):        # Glance a message
             response += "\n*By Month : *" + "*" + most_profitable_by_month['coin'] + "*" + "\n" + "Percentage : " + str(round(most_profitable_by_month['diff'],2)) + "%"
             response += "\n*By Year : *" + "*" + most_profitable_by_year['coin'] + "*" + "\n" + "Percentage : " + str(round(most_profitable_by_year['diff'],2)) + "%"
             bot.sendMessage(chat_id, response, parse_mode='Markdown')
-        elif previous_message == "/checkprice":
+        elif previous_message == "/price":
             # message  = message.split(" ")
             # coin = message[1].upper()
             coin = message.upper()
